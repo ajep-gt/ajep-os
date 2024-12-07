bacde69657c802e0fec2ed069d7345868d6ff249
ajep-gt <72942924+ajep-gt@users.noreply.github.com>
2024-12-07 08:28:58 -0600

    Kinoite image

---
+++ files/scripts/rpmostreeoverride.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
+rpm-ostree override remove \
+  ffmpeg-free \
+  libavcodec-free \
+  libavdevice-free \
+  libavfilter-free \
+  libavformat-free \
+  libavutil-free \
+  libpostproc-free \
+  libswresample-free \
+  libswscale-free \
+  --install ffmpeg
